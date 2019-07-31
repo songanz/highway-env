6e5f255ff4347a73357162e8f9b89990fbaa5880
Songan Zhang <songanz@umich.edu>
2019-07-31 12:40:38 -0400

    Re-organized

--- scripts/bash_for_ford_hpc/Surprise_dis_IDM_00.sh
+++ scripts/bash_for_ford_hpc/Surprise_dis_IDM_00.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /s/szhan117/highway-env/
 export PYTHONPATH=/s/szhan117/highway-env/
-python3 ./scripts/train_surprise.py --env=highway-discrete-v0 --save_path=/s/szhan117/highway-env/models/Surprise_dis/00 --env_json=/s/szhan117/highway-env/scripts/config/IDM.json
+python3 ./scripts/train_surprise.py --env=highway-discrete-v0 --save_path=/s/szhan117/highway-env/models/Surprise_dis/00/latest --env_json=/s/szhan117/highway-env/scripts/config/IDM.json
--- scripts/bash_for_ford_hpc/Surprise_dis_aggressive_00.sh
+++ scripts/bash_for_ford_hpc/Surprise_dis_aggressive_00.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /s/szhan117/highway-env/
 export PYTHONPATH=/s/szhan117/highway-env/
-python3 ./scripts/train_surprise.py --env=highway-discrete-v0 --save_path=/s/szhan117/highway-env/models/Surprise_dis_aggressive/00 --env_json=/s/szhan117/highway-env/scripts/config/Aggressive.json
+python3 ./scripts/train_surprise.py --env=highway-discrete-v0 --save_path=/s/szhan117/highway-env/models/Surprise_dis_aggressive/00/latest --env_json=/s/szhan117/highway-env/scripts/config/Aggressive.json
