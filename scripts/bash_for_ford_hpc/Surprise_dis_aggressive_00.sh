ec0768699343d95cacf6125cef5d493579d41473
Songan Zhang <songanz@umich.edu>
2019-07-31 10:28:32 -0400

    Fix bugs

--- scripts/bash_for_ford_hpc/Surprise_dis_aggressive_00.sh
+++ scripts/bash_for_ford_hpc/Surprise_dis_aggressive_00.sh
@@ -1,3 +1,3 @@
 cd /s/szhan117/highway-env/
 export PYTHONPATH=/s/szhan117/highway-env/
-python3 ,/scripts/train_surprise.py --env=highway-discrete-v0 --save_path=/s/szhan117/highway-env/models/Surprise_dis_aggressive/latest --env_json_path=/s/szhan117/highway-env/scripts/config/Aggressive.json
+python3 ./scripts/train_surprise.py --env=highway-discrete-v0 --save_path=/s/szhan117/highway-env/models/Surprise_dis_aggressive/00 --env_json=/s/szhan117/highway-env/scripts/config/Aggressive.json
