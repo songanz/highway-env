315170a22dddd201653c35dc1fad77eede2c6edb
Songan Zhang <songanz@umich.edu>
2019-08-01 13:15:04 -0400

    !!!!!!Changed MDPVehicle for discrete env
    Need to re-run all the simulation in discrete env

---
+++ scripts/bash_for_ford_hpc/Surprise_dis_aggressive_01.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /s/szhan117/highway-env/
+export PYTHONPATH=/s/szhan117/highway-env/
+python3 ./scripts/train_surprise.py \
+--env=highway-discrete-intrinsic-rew-v0 \
+--save_path=/s/szhan117/highway-env/models/Surprise_dis_aggressive/01/latest \
+--env_json=/s/szhan117/highway-env/scripts/config/Aggressive.json
---
+++ scripts/bash_for_ford_hpc/Surprise_dis_aggressive_03.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /s/szhan117/highway-env/
+export PYTHONPATH=/s/szhan117/highway-env/
+python3 ./scripts/train_surprise.py \
+--env=highway-discrete-intrinsic-rew-v0 \
+--save_path=/s/szhan117/highway-env/models/Surprise_dis_aggressive/03/latest \
+--env_json=/s/szhan117/highway-env/scripts/config/Aggressive.json
