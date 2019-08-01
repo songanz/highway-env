58aa551c732f2bc367b1c277248a876ee4d47901
Songan Zhang <songanz@umich.edu>
2019-08-01 11:33:54 -0400

    Update bash for home

--- scripts/bash_for_ford_hpc/imagine_dis_IDM2Agg_wIDM_model_01.sh
+++ scripts/bash_for_ford_hpc/imagine_dis_IDM2Agg_wIDM_model_01.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-cd /s/szhan117/highway-env/
-export PYTHONPATH=/s/szhan117/highway-env/
+cd /home/songanz/Documents/Git_repo/highway-env/
+export PYTHONPATH=/home/songanz/Documents/Git_repo/highway-env/
 python3 ./scripts/train_imagine.py \
 --env=highway-discrete-imagine-v0 \
---save_path=/s/szhan117/highway-env/models/Imagine_dis_IDM2Agg_wIDM_model/01/latest \
---env_json=/s/szhan117/highway-env/scripts/config/im_Aggressive.json \
---load_path=/s/szhan117/highway-env/models/baseline_dis/00/latest
+--save_path=/home/songanz/Documents/Git_repo/highway-env/models/Imagine_dis_IDM2Agg_wIDM_model/01/latest \
+--env_json=/home/songanz/Documents/Git_repo/highway-env/scripts/config/im_Aggressive.json \
+--load_path=/home/songanz/Documents/Git_repo/highway-env/models/baseline_dis/00/latest
