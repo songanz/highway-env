474d1f0fe6d6e0b54d9d83ccb80c943a7f2019af
Songan Zhang <songanz@umich.edu>
2019-08-01 20:30:50 -0400

    Fixed bug

--- scripts/bash_for_ford_hpc/baseline_dis_aggressive_04.sh
+++ scripts/bash_for_ford_hpc/baseline_dis_aggressive_04.sh
@@ -3,5 +3,5 @@ cd /s/szhan117/highway-env/
 export PYTHONPATH=/s/szhan117/highway-env/
 python3 ./scripts/train_baselines.py \
 --env=highway-discrete-v0 \
---save_path=/s/szhan117/highway-env/models/baseline_dis_aggressive/00/latest \
+--save_path=/s/szhan117/highway-env/models/baseline_dis_aggressive/04/latest \
 --env_json=/s/szhan117/highway-env/scripts/config/Aggressive.json
