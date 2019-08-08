cb3612a4c447d74de3d8131679d22834970d7c70
Songan Zhang <songanz@umich.edu>
2019-08-08 10:23:04 -0400

    Re-organized scripts

--- scripts/bash_for_ford_hpc/trpo/baseline_con_aggressive.sh
+++ scripts/bash_for_ford_hpc/trpo/baseline_con_aggressive.sh
@@ -3,5 +3,6 @@ cd /s/szhan117/highway-env/
 export PYTHONPATH=/s/szhan117/highway-env/
 python3 ./scripts/train.py \
 --env=highway-continuous-v0 \
---save_path=/s/szhan117/highway-env/models/baseline_con_aggressive/00/latest \
+--save_path=/s/szhan117/highway-env/models/trpo/baseline_con_aggressive/00/latest \
 --env_json=/s/szhan117/highway-env/scripts/config/Aggressive.json
+--alg=trpo_mpi
