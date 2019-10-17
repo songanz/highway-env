4bd542a7cfa7fec08f1fe67ef1e67272a1ee1e9f
Songan Zhang <songanz@umich.edu>
2019-10-17 11:32:18 -0400

    little change

--- scripts/bash_stabel_baseline/SAC/Surprise_dis_IDM.sh
+++ scripts/bash_stabel_baseline/SAC/Surprise_dis_IDM.sh
@@ -3,6 +3,6 @@ cd /home/songan/Documents/git_repo/highway-env/
 export PYTHONPATH=/home/songan/Documents/git_repo/highway-env/
 python3 ./scripts/train.py \
 --env=highway-discrete-intrinsic-rew-v0 \
---save_path=/home/songan/Documents/git_repo/highway-env/trails/sac/Surprise_dis_IDM/00/latest \
+--save_path=/home/songan/Documents/git_repo/highway-env/trails/sac/Surprise_dis/00/latest \
 --env_json=/home/songan/Documents/git_repo/highway-env/scripts/config/IDM.json
 --alg=sac
--- scripts/bash_stabel_baseline/SAC/baseline_dis_IDM.sh
+++ scripts/bash_stabel_baseline/SAC/baseline_dis_IDM.sh
@@ -3,6 +3,6 @@ cd /home/songan/Documents/git_repo/highway-env/
 export PYTHONPATH=/home/songan/Documents/git_repo/highway-env/
 python3 ./scripts/train.py \
 --env=highway-discrete-v0 \
---save_path=/home/songan/Documents/git_repo/highway-env/trails/sac/baseline_dis_IDM/00/latest \
+--save_path=/home/songan/Documents/git_repo/highway-env/trails/sac/baseline_dis/00/latest \
 --env_json=/home/songan/Documents/git_repo/highway-env/scripts/config/IDM.json
 --alg=sac
